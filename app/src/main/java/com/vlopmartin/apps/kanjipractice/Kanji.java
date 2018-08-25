817eff18b30183c286e00e361bd9d7cbcdd80032
Víctor López <vlopmartin@gmail.com>
2018-08-25 20:52:40 +0200

    Added database access methods to Kanji class

--- app/src/main/java/com/vlopmartin/apps/kanjipractice/Kanji.java
+++ app/src/main/java/com/vlopmartin/apps/kanjipractice/Kanji.java
@@ -1,5 +1,13 @@
 package com.vlopmartin.apps.kanjipractice;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kanji {
 
     public static final String createTableSQL = "CREATE TABLE KANJIS (" +
@@ -23,4 +31,70 @@ public class Kanji {
     private int set;
     public int getSet() { return set; }
     public void setSet(int set) { this.set = set; }
+
+    public Kanji(long id, String read, String written, int set) {
+        this.setId(id);
+        this.setRead(read);
+        this.setWritten(written);
+        this.setSet(set);
+    }
+
+    public void save(Context ctx) {
+        SQLiteDatabase db = new DBHelper(ctx).getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        if (this.id != 0) {
+            values.put("ID", this.getId());
+        }
+        values.put("READ", this.getRead());
+        values.put("WRITTEN", this.getWritten());
+        values.put("SET", this.getSet());
+
+        long id = db.replace("KANJIS", null, values);
+        this.setId(id);
+    }
+
+    public void delete(Context ctx) {
+        SQLiteDatabase db = new DBHelper(ctx).getWritableDatabase();
+
+        db.delete("KANJIS", "ID = ?", new String[] {String.valueOf(this.getId())});
+    }
+
+    private static Kanji readCursor(Cursor cursor) {
+        long id = cursor.getLong(cursor.getColumnIndex("ID"));
+        String read = cursor.getString(cursor.getColumnIndex("READ"));
+        String written = cursor.getString(cursor.getColumnIndex("WRITTEN"));
+        int set = cursor.getInt(cursor.getColumnIndex("SET"));
+        return new Kanji(id, read, written, set);
+    }
+
+    public static Kanji getById(Context ctx, long id) {
+        Kanji ret;
+
+        SQLiteDatabase db = new DBHelper(ctx).getReadableDatabase();
+        Cursor cursor = db.query("KANJIS", null, "id = ?", new String[] {String.valueOf(id)}, null, null, null);
+
+        if (cursor.moveToNext()) {
+            ret = readCursor(cursor);
+        } else {
+            ret = null;
+        }
+
+        cursor.close();
+        return ret;
+    }
+
+    public static List<Kanji> getSet(Context ctx, int set) {
+        List<Kanji> ret = new ArrayList<Kanji>();
+
+        SQLiteDatabase db = new DBHelper(ctx).getReadableDatabase();
+        Cursor cursor = db.query("KANJIS", null, "set = ?", new String[] {String.valueOf(set)}, null, null, null);
+
+        while (cursor.moveToNext()) {
+            ret.add(readCursor(cursor));
+        }
+
+        cursor.close();
+        return ret;
+    }
 }
