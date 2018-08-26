a6c06de25b5d04bba5e8dc9da1703b275679475b
Víctor López <vlopmartin@gmail.com>
2018-08-26 11:23:54 +0200

    Changed kanji list icons and added Add button

--- app/src/main/java/com/vlopmartin/apps/kanjipractice/activities/EditPracticeSetActivity.java
+++ app/src/main/java/com/vlopmartin/apps/kanjipractice/activities/EditPracticeSetActivity.java
@@ -1,9 +1,11 @@
 package com.vlopmartin.apps.kanjipractice.activities;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.vlopmartin.apps.kanjipractice.Kanji;
 import com.vlopmartin.apps.kanjipractice.R;
@@ -18,19 +20,34 @@ public class EditPracticeSetActivity extends AppCompatActivity implements KanjiF
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_practice_set);
 
+        setFragments();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setFragments();
+    }
+
+    protected void setFragments() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if (globalSetFragment != null) transaction.remove(globalSetFragment);
+        if (practiceSetFragment != null) transaction.remove(practiceSetFragment);
         globalSetFragment = KanjiFragment.newInstance(0);
         practiceSetFragment = KanjiFragment.newInstance(0);
-
-        // Add fragments
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.globalSetFrame, globalSetFragment);
         transaction.add(R.id.practiceSetFrame, practiceSetFragment);
         transaction.commit();
-
     }
 
     @Override
     public void onListFragmentInteraction(Kanji kanjiItem) {
 
+    }
+
+    public void onAddKanji(View v) {
+        Intent intent = new Intent(this, NewKanjiActivity.class);
+        this.startActivity(intent);
     }
 }
